e11a8a7712fd646450eccf691c613e0457bf9c39
Munish Kumar <mk6229478@gmail.com>
2020-05-05 13:00:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
+/*
 $conn=mysqli_connect('localhost','root','','iti');
 if(!$conn){
 	echo "Not Connected";
 }
 else{
-	header("location:home.php");
 }
+*/
 
+header("location:home.php");
 ?>
