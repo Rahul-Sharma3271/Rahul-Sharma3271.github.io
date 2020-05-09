bd7c432136a0b4c3e56bb021789420ee4216e858
Munish Kumar <mk6229478@gmail.com>
2020-05-09 14:58:51 +0530

    Update logindb.php

--- logindb.php
+++ logindb.php
@@ -15,23 +15,19 @@
 			$sql = "SELECT * FROM registerdb WHERE uname='$uname' AND password='$pwd'";
 
 			$result = mysqli_query($conn, $sql); 
-			//$status = mysqli_num_rows($result); //true for 1, false for 0 or 2
-			//if($result){
-				if (mysqli_num_rows($result) == 1) {
+			$status = mysqli_num_rows($result); //true for 1, false for 0 or 2
+			
+				if ($status == 1) {
 					$_SESSION['login_user'] = $uname;
 					// echo "DOnee";
 					//header('Location: member.php');
-					echo"Login Sucess";			
+					echo"Login Success";			
 				}
 				else {
 					echo $error = "Your Login Name or Password is invalid";
 					echo"Failed";
 					//header('Location: client.php');
 				}
-			//}
-			//else{
-			//	echo "No User";
-			//}
 		}
 
 
